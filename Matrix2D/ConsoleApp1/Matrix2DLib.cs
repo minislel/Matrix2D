d44e476fc675fd85902716ece4fb92b38d6dd4eb
minislel <marcin4kt@gmail.com>
2024-03-21 21:15:42 +0100

    Added arithmetic operators

--- Matrix2D/ConsoleApp1/Matrix2DLib.cs
+++ Matrix2D/ConsoleApp1/Matrix2DLib.cs
@@ -19,14 +19,14 @@ namespace Matrix2DConsoleApp
                 return new Matrix2D();
             }
         }
-        public static  Matrix2D Zero
+        public static Matrix2D Zero
         {
             get
             {
                 return new Matrix2D(0, 0, 0, 0);
             }
         }
-    
+
         public Matrix2D(int a, int b, int c, int d)
         {
             A = a;
@@ -34,12 +34,12 @@ namespace Matrix2DConsoleApp
             C = c;
             D = d;
         }
-        public Matrix2D() 
-        { 
-            A=1;
-            B=0;
-            C=0;
-            D=1;
+        public Matrix2D()
+        {
+            A = 1;
+            B = 0;
+            C = 0;
+            D = 1;
         }
         public override string ToString()
         {
@@ -48,24 +48,49 @@ namespace Matrix2DConsoleApp
 
         public bool Equals(Matrix2D? other)
         {
-        if (other == null) return false;
-            if(this.A == other.A && this.B == other.B && this.C == other.C && this.D == other.D)
-            { 
+            if (other == null) return false;
+            if (this.A == other.A && this.B == other.B && this.C == other.C && this.D == other.D)
+            {
                 return true;
             }
-            else 
+            else
             {
                 return false;
             }
         }
         public override bool Equals(object? obj)
-        { 
-            return Equals(obj as Matrix2D); 
+        {
+            return Equals(obj as Matrix2D);
         }
         public override int GetHashCode()
-        { 
+        {
             return A.GetHashCode() ^ B.GetHashCode() ^ C.GetHashCode() ^ D.GetHashCode();
         }
-        
+        public static Matrix2D operator +(Matrix2D m1, Matrix2D m2)
+        {
+            return new Matrix2D(m1.A+m2.A, m1.B+m2.B, m1.C+m2.C, m1.D+m2.D);
+        }
+        public static Matrix2D operator -(Matrix2D m1, Matrix2D m2)
+        {
+            return new Matrix2D(m1.A - m2.A, m1.B - m2.B, m1.C - m2.C, m1.D - m2.D);
+        }
+        public static Matrix2D operator *(Matrix2D m1, Matrix2D m2)
+        {
+            return new Matrix2D(m1.A * m2.A, m1.B * m2.B, m1.C * m2.C, m1.D * m2.D);
+        }
+        public static Matrix2D operator *(Matrix2D m, int k)
+        {
+            return new Matrix2D(m.A * k, m.B*k, m.C*k, m.D*k);
+        }
+        public static Matrix2D operator *(int k, Matrix2D m)
+        {
+            return new Matrix2D(m.A * k, m.B * k, m.C * k, m.D * k);
+        }
+        public static Matrix2D operator -(Matrix2D m)
+        {
+            return new Matrix2D(m.A * -1, m.B * -1, m.C * -1, m.D * -1);
+        }
+
+
     }
 }
